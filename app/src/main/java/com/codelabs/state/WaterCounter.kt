8a00afed6042f85816f2f54230a07bf8958c22ac
DiegoDZ243 <s23120243@alumnos.itsur.edu.mx>
2026-09-23 16:49:35 -0600

    Elevacion de estado de la función WaterCounter

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -31,40 +31,46 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+//@Composable
+//fun StatefulCounter(modifier: Modifier = Modifier) {
+//    var count by rememberSaveable { mutableStateOf(0) }
+//    StatelessCounter(
+//        count = count,
+//        onIncrement = { count++ },
+//        modifier = modifier
+//    )
+//}
+//
+//@Composable
+//fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+//    Column(modifier = modifier.padding(16.dp)) {
+//        if (count > 0) {
+//            Text("You've had $count glasses.")
+//        }
+//        Button(
+//            onClick = onIncrement,
+//            enabled = count < 10,
+//            modifier = Modifier.padding(top = 8.dp)
+//        ) {
+//            Text("Add one")
+//        }
+//    }
+//}
+
+
 @Composable
-fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by rememberSaveable { mutableStateOf(0) }
-    StatelessCounter(
-        count = count,
-        onIncrement = { count++ },
-        modifier = modifier
-    )
+fun WaterCounterStateful(modifier: Modifier= Modifier){
+    var count by rememberSaveable { mutableStateOf(0)}
+    WaterCounter(count, {count++}, modifier)
 }
 
 @Composable
-fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+fun WaterCounter(count:Int,onIncrement:()->Unit,modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(
-            onClick = onIncrement,
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
-        }
-    }
-}
-
-@Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
--- app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -20,27 +20,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-@Composable
-fun WellnessScreen(
-    modifier: Modifier = Modifier,
-    wellnessViewModel: WellnessViewModel = viewModel()
-) {
-    Column(modifier = modifier) {
-        StatefulCounter()
-
-        WellnessTasksList(
-            list = wellnessViewModel.tasks,
-            onCheckedTask = { task, checked ->
-                wellnessViewModel.changeTaskChecked(task, checked)
-            },
-            onCloseTask = { task ->
-                wellnessViewModel.remove(task)
-            }
-        )
-    }
-}
+//@Composable
+//fun WellnessScreen(
+//    modifier: Modifier = Modifier,
+//    wellnessViewModel: WellnessViewModel = viewModel()
+//) {
+//    Column(modifier = modifier) {
+//        StatefulCounter()
+//
+//        WellnessTasksList(
+//            list = wellnessViewModel.tasks,
+//            onCheckedTask = { task, checked ->
+//                wellnessViewModel.changeTaskChecked(task, checked)
+//            },
+//            onCloseTask = { task ->
+//                wellnessViewModel.remove(task)
+//            }
+//        )
+//    }
+//}
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    WaterCounterStateful()
 }
